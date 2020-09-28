ffeaf93208bbbeed653e3e69febbb385aecf6900
harsharay <harsharay.ray@gmail.com>
2020-09-29 01:24:19 +0530

    Updates

---
+++ precache-manifest.957fe28012c66dda708426f1db7ac59e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab40577984f46fb8867903a4aef0e5d7",
+    "url": "/revvos/index.html"
+  },
+  {
+    "revision": "e605da976ce50f07f1f9",
+    "url": "/revvos/static/css/main.8579d73a.chunk.css"
+  },
+  {
+    "revision": "19250e2fb84f975ae76a",
+    "url": "/revvos/static/js/2.3fc3e831.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/revvos/static/js/2.3fc3e831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e605da976ce50f07f1f9",
+    "url": "/revvos/static/js/main.1bc60a1f.chunk.js"
+  },
+  {
+    "revision": "90827a1136e6039d76f6",
+    "url": "/revvos/static/js/runtime-main.34a1a21a.js"
+  }
+]);
